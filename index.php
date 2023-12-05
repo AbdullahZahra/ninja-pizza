9cb446b647fbbb8c481b28ab03bd1756b87f29c7
Abdullah Zahra <abdullahmzahra@gmail.com>
2023-12-05 06:14:31 +0200

    Variables & Constants

--- index.php
+++ index.php
@@ -1,15 +1,27 @@
 <?php
 
+define('NAME', 'Yoshi');
 
+// $name = 'Yoshi';
+$age = 30;
+
+// $name = 'Mario';
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My First PHP File</title>
+</head>
+<body>
+    
+    <h1>User Profile Page</h1>
 
-<?php include('templates/header.php') ?>
+    <div><?php echo NAME ?></div>
+    <div><?php echo $age; ?></div>
 
-
-<?php include('templates/footer.php') ?>
-
+</body>
 </html>
--- templates/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="section">
-    <div class="center grey-text">Copyright 2024 Ninja Pizzas</div>
-</footer>
-
-</body>
